ee3ae72534a137dfd9cc4b8df2ff395bbb60b2bf
MICHON_Home <francois-xavier.michon@proton.me>
2026-08-07 12:00:59 +0200

    modif code

--- js/main.js
+++ js/main.js
@@ -3,4 +3,12 @@ import AnimateOnView from "./components/animateOnView.js";
 document.addEventListener("DOMContentLoaded", () => {
     console.log("MAIN JS LOADED");
     new AnimateOnView(".animated-image", { threshold: 0.4 });
+
+    document.querySelectorAll('.mediaBlockMedia').forEach(img => {
+    img.onload = () => {
+        img.style.aspectRatio = `${img.naturalWidth} / ${img.naturalHeight}`;
+    };
 });
+});
+
+
